3415d18b32eb45204b5d2da4656de31b454ea87c
msh1273 <mch1273@naver.com>
2021-08-24 02:11:15 +0900

    체육복

---
+++ gymSuit.py
@@ -0,0 +1,19 @@
+n = 3
+lost = [3]
+reserve = [1]
+
+# 1.lost배열과 reserve배열에 같은 학생이 있는경우 두 배열에서 제거한다. (여벌옷이 있지만 도난당한 학생)
+
+def solution(n, lost, reserve):
+    # 두 리스트의 서로 중복되는 부분 제거
+    setLost = list(set(lost) - set(reserve))
+    setReserve = list(set(reserve) - set(lost))
+    popLost = setLost   # 최종 잃어버린 학생을 담을 리스트
+    for i in setReserve:
+            if (i-1 in setLost):
+                setLost.remove(i-1)
+            elif (i+1 in setLost):
+                setLost.remove(i+1)
+    return n - len(setLost)
+
+print(solution(n, lost, reserve))
