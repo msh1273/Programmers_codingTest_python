a61b53a5948047eb5d137f138663c002728f7531
msh1273 <mch1273@naver.com>
2021-08-25 21:31:50 +0900

    add 위장 풀이

--- camouflage.py
+++ camouflage.py
@@ -1,5 +1,4 @@
-from typing import AnyStr
-
+from collections import Counter
 
 clothes = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
 
@@ -19,5 +18,15 @@ def solution(clothes):
         answer *= (len(type[key]) + 1)
     return answer-1
 
+# 추가) Counter를 사용한 풀이
+def solution1(clothes):
+    answer = 1 
+    c = Counter(key[1] for key in clothes) #clothes의 종류별 갯수를 dic 형태로 변환
+    
+    for i in c.values():
+        answer *= i+1
+    answer -= 1
+    return answer
 
-print(solution(clothes))
+
+print(solution1(clothes))
