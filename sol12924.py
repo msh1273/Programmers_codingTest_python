9d4171089b03b89c20f1f202178a49d13849060e
msh1273 <mch1273@naver.com>
2021-10-07 00:26:52 +0900

    숫자의 표현 (효율성 실패)

--- sol12924.py
+++ sol12924.py
@@ -2,7 +2,17 @@ n = 15
 
 def solution(n):
     count = 0
-    start = 1
-    for i in range(start,n+1):
-        
-    return answer
+    for i in range(1, n+1):
+        if checkFinn(i, n):
+            count += 1
+    return count
+
+def checkFinn(start, n):
+    _sum = 0
+    for i in range(start, n+1):
+        _sum += i
+        if _sum == n:
+            return True
+    return False
+
+print(solution(n))
