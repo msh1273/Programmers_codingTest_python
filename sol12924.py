9672cdc84dd0d0cb9cea16e57c2283c0deb550a0
msh1273 <mch1273@naver.com>
2021-10-07 00:40:00 +0900

    숫자의 표현

--- sol12924.py
+++ sol12924.py
@@ -3,16 +3,15 @@ n = 15
 def solution(n):
     count = 0
     for i in range(1, n+1):
-        if checkFinn(i, n):
-            count += 1
+        _sum = 0
+        for j in range(i, n+1):
+            _sum += j
+            if _sum == n:
+                count += 1
+                break
+            elif _sum > n:
+                break
     return count
 
-def checkFinn(start, n):
-    _sum = 0
-    for i in range(start, n+1):
-        _sum += i
-        if _sum == n:
-            return True
-    return False
 
 print(solution(n))
